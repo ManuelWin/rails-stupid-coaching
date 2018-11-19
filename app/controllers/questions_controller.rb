17682b0771077e93bb8979646bb9bf5badb6cdad
Manuel Winzler <manu_elco@hotmail.de>
2018-11-19 18:21:32 -0200

    exercise 1 stupid coaching working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,12 +4,16 @@ class QuestionsController < ApplicationController
 
   def answer
     @input = params[:user_input]
-    if @input == 'I am going to work'
-      @answer == 'Great!'
-    elsif @input.inlcude? '?'
-      @answer == 'Silly question, get dressed and got o work!'
+    if @input.blank?
+      @answer = "I can't hear you!"
+    elsif @input =='I am going to work'
+      @answer = 'Great!'
+    elsif @input.ends_with?("?")
+      @answer = 'Silly question, get dressed and got o work!'
     else
-      @answer == 'I do not care, get dressed and go to work!'
+      @answer = 'I do not care, get dressed and go to work!'
     end
   end
 end
+
+
